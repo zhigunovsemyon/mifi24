4584e209e58ba83bb72fbf682d55ce166e94df6d
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-05-15 20:50:27 +0300

    Добавил прототипы инита листа, добавления одного элемента и массива

--- src/list.h
+++ src/list.h
@@ -13,4 +13,14 @@ typedef struct {
 	pEntry *head;
 } List;
 
+List list_init();
+
+void list_free(List *);
+
+/*Вставка одного элемента. При ошибке возвращает true*/
+bool list_append(List *, int);
+
+/*Вставка массива элементов. При ошибке возвращает true*/
+bool list_append_range(List *, int const *, size_t);
+
 #endif // !LIST_H
